d053a3260fe381a6fce3fa039278143cceb3e8d8
Michael Stallings <stall84@gmail.com>
2022-10-02 17:07:22 -0400

    Got code setup and quickly started the netcat server to test..

---
+++ index.py
@@ -0,0 +1,45 @@
+import sys
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print('Usage: struc_stream_net_wordcount.py <hostname> <port>', file=sys.stderr)
+        sys.exit(-1)
+
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+
+    spark = SparkSession\
+        .builder\
+        .appName('Stuctured_Network_WordCount')\
+        .getOrCreate()
+
+    # Create the DataFrame which will represent the stream of input lines from connection to host:port
+    lines = spark\
+        .readStream\
+        .format('socket')\
+        .option('host', host)\
+        .option('port', port)\
+        .load()
+
+    # Split lines into words
+    words = lines.select(
+        # explode turns each item in an array into a separate row
+        explode(
+            split(lines.value, ' ')
+        ).alias('word')
+    )
+
+    # Generate running word count
+    wordCounts = words.groupBy('word').count()
+
+    # Start running the query that prints the running counts to the console
+    query = wordCounts\
+        .writeStream\
+        .outputMode('complete')\
+        .format('console')\
+        .start()
+
+    query.awaitTermination()
